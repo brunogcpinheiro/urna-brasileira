8efce36c2bb7ca340eeb66d6b9336ad8e8d3bb13
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-10-09 14:44:34 -0300

    mostrando dados do estado do componente App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React, { Component } from "react";
-import Urna from "./components/Urna/Urna";
+import React, { Component } from 'react';
+import Urna from './components/Urna/Urna';
 
-import styles from "./App.module.css";
+import styles from './App.module.css';
 
 class App extends Component {
+  state = {
+    candidatos: [
+      {
+        id: '17',
+        nome: 'Jair Messias Bolsonaro',
+        partido: 'PSL',
+        vice: 'General Hamilton Mourão',
+        imagem:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWNai_ski7BCALwuUflTtzVDMqNDrQ0ivskjqV2qIGxYeuLf1SQg',
+        imagemVice:
+          'https://static.congressoemfoco.uol.com.br/2018/10/general-hamilton-mourao-pedro-ribas-anpr.jpeg'
+      }
+      // {
+      //   id: '13',
+      //   nome: 'Fernando Haddad',
+      //   partido: 'PT',
+      //   vice: "Manuela D'Ávila",
+      //   imagem:
+      //     'https://www.cartacapital.com.br/revista/1020/fernando-haddad-vamos-retomar-o-projeto-de-Lula/Fernando%20Haddad.jpg/@@images/b1362c58-14b0-4bb9-b21b-67bc0ff74b24.jpeg',
+      //   imagemVice:
+      //     'https://abrilveja.files.wordpress.com/2018/06/brasil-politica-debete-pre-candidatos-brasilia-20180606-0001-copy.jpg'
+      // }
+    ]
+  };
+
   render() {
+    const { candidatos } = this.state;
+
     return (
       <div className={styles.App}>
         <h1>
           Urna Brasileira para <span>Leigos</span>
         </h1>
-        <Urna />
+        <Urna candidatos={candidatos} />
       </div>
     );
   }
--- src/components/Urna/Urna.js
+++ src/components/Urna/Urna.js
@@ -6,61 +6,63 @@ class Urna extends Component {
   state = {};
 
   render() {
+    const { candidatos } = this.props;
     return (
       <div className={styles.Urna}>
-        <div className={styles.UrnaTela}>
-          <div className={styles.Entrada}>
-            <p>SEU VOTO PARA:</p>
-            <h2>PRESIDENTE</h2>
-            <div className={styles.Digitos}>
-              <p>Número:</p>
-              <div className={styles.Digito}>
-                <input type="text" maxLength="1" />
+        {candidatos.map(candidato => (
+          <div className={styles.UrnaTela}>
+            <div className={styles.Entrada}>
+              <p>SEU VOTO PARA:</p>
+              <h2>PRESIDENTE</h2>
+              <div className={styles.Digitos}>
+                <p>Número:</p>
+                <div className={styles.Digito}>
+                  <input type="text" maxLength="1" />
+                </div>
+                <div className={styles.Digito}>
+                  <input type="text" maxLength="1" />
+                </div>
               </div>
-              <div className={styles.Digito}>
-                <input type="text" maxLength="1" />
+              <div className={styles.Dados}>
+                <div>
+                  <p>
+                    Nome: <strong>{candidato.nome}</strong>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Partido: <strong>{candidato.partido}</strong>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Vice-presidente: <strong>{candidato.vice}</strong>
+                  </p>
+                </div>
+              </div>
+              <div className={styles.Instrucoes}>
+                <p>
+                  Para CONFIRMAR este voto tecle <strong>CONFIRMA</strong>.
+                </p>
+                <p>
+                  Para REINICIAR este voto tecle <strong>CORRIGE</strong>.
+                </p>
               </div>
             </div>
-            <div className={styles.Dados}>
-              <div>
-                <p>
-                  Nome: <strong>Jair Messias Bolsonaro</strong>
-                </p>
+            <div className={styles.Informacoes}>
+              <div className={styles.Candidato}>
+                <img alt="foto" src={candidato.imagem} />
+                <img alt="foto" src={candidato.imagemVice} />
               </div>
-              <div>
-                <p>
-                  Partido: <strong>PSL</strong>
-                </p>
-              </div>
-              <div>
-                <p>
-                  Vice-presidente: <strong>General Hamilton Mourão</strong>
-                </p>
-              </div>
-            </div>
-            <div className={styles.Instrucoes}>
-              <p>
-                Para CONFIRMAR este voto tecle <strong>CONFIRMA</strong>.
-              </p>
-              <p>
-                Para REINICIAR este voto tecle <strong>CORRIGE</strong>.
-              </p>
             </div>
           </div>
-          <div className={styles.Informacoes}>
-            <div className={styles.Candidato}>
-              <img
-                alt="foto"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWNai_ski7BCALwuUflTtzVDMqNDrQ0ivskjqV2qIGxYeuLf1SQg"
-              />
-              <img
-                alt="foto"
-                src="https://static.congressoemfoco.uol.com.br/2018/10/general-hamilton-mourao-pedro-ribas-anpr.jpeg"
-              />
-            </div>
-          </div>
+        ))}
+        <div className={styles.UrnaTeclado}>
+          <div
+            style={{ background: '#f5f5f5', width: '100%', height: '80px' }}
+          />
+          <div className={styles.Teclas} />
         </div>
-        <div className={styles.UrnaTeclado} />
       </div>
     );
   }
