a5478974da9ea5f75bee36d91570979efad460f6
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-10-10 08:15:16 -0300

    inserindo numeros nos dois campos

--- src/components/Urna/Urna.js
+++ src/components/Urna/Urna.js
@@ -12,17 +12,34 @@ class Urna extends Component {
   handleResultTemp = () => {
     const final = this.state.primeiroNumero + this.state.segundoNumero;
     this.setState({ finalNumero: final });
+    console.log(final);
   };
 
-  handleNumber1 = query => {
+  handleNumber = query => {
+    if (!this.state.primeiroNumero) {
+      this.setState({
+        primeiroNumero: query
+      });
+    } else {
+      this.setState({
+        segundoNumero: query
+      });
+    }
+  };
+
+  handleInput = e => {
     this.setState({
-      primeiroNumero: query
+      primeiroNumero: e.target.value
     });
+
+    this.nameInput.focus();
   };
 
-  handleNumber2 = query => {
+  clearInputs = query => {
     this.setState({
-      segundoNumero: query
+      primeiroNumero: query,
+      segundoNumero: query,
+      finalNumero: query
     });
   };
 
@@ -44,15 +61,19 @@ class Urna extends Component {
                     type="text"
                     maxLength="1"
                     value={primeiroNumero}
-                    onChange={e =>
-                      this.setState({ primeiroNumero: e.target.value })
-                    }
+                    // onChange={e =>
+                    //   this.setState({ primeiroNumero: e.target.value })
+                    // }
+                    onChange={this.handleInput}
                   />
                 </div>
                 <div className={styles.Digito}>
                   <input
                     type="text"
                     maxLength="1"
+                    ref={input => {
+                      this.nameInput = input;
+                    }}
                     value={segundoNumero}
                     onChange={e =>
                       this.setState({ segundoNumero: e.target.value })
@@ -108,30 +129,83 @@ class Urna extends Component {
           <div className={styles.Teclas}>
             <button
               className={styles.Numeros}
-              onClick={e => this.handleNumber1("1")}
+              name="1"
+              onClick={e => this.handleNumber(e.target.name)}
             >
               1
             </button>
-            <button className={styles.Numeros}>2</button>
-            <button className={styles.Numeros}>3</button>
-            <button className={styles.Numeros}>4</button>
-            <button className={styles.Numeros}>5</button>
-            <button className={styles.Numeros}>6</button>
             <button
               className={styles.Numeros}
-              onClick={e => this.handleNumber2("7")}
+              name="2"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              2
+            </button>
+            <button
+              className={styles.Numeros}
+              name="3"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              3
+            </button>
+            <button
+              className={styles.Numeros}
+              name="4"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              4
+            </button>
+            <button
+              className={styles.Numeros}
+              name="5"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              5
+            </button>
+            <button
+              className={styles.Numeros}
+              name="6"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              6
+            </button>
+            <button
+              className={styles.Numeros}
+              name="7"
+              onClick={e => this.handleNumber(e.target.name)}
             >
               7
             </button>
-            <button className={styles.Numeros}>8</button>
-            <button className={styles.Numeros}>9</button>
-            <button className={styles.Numeros}>0</button>
+            <button
+              className={styles.Numeros}
+              name="8"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              8
+            </button>
+            <button
+              className={styles.Numeros}
+              name="9"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              9
+            </button>
+            <button
+              className={styles.Numeros}
+              name="0"
+              onClick={e => this.handleNumber(e.target.name)}
+            >
+              0
+            </button>
           </div>
           <div className={styles.Acoes}>
             <button onClick={this.handleResultTemp} className={styles.Branco}>
               BRANCO
             </button>
-            <button onClick={this.handleResultTemp} className={styles.Corrige}>
+            <button
+              onClick={e => this.clearInputs("")}
+              className={styles.Corrige}
+            >
               CORRIGE
             </button>
             <button onClick={this.handleResultTemp} className={styles.Confirma}>
